a37f4a28f38d84c739459488c04dfcab20509a39
unknown <i.p.m.a.n.03.31.87@gmail.com>
2020-05-11 13:41:29 -0500

    first commit

---
+++ AsyncDelegatesAndThreading/UsingAThread.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreadsAndDelegates
+{
+    public partial class UsingAThread : Form
+    {
+        int _Max;
+        delegate void StartProcessHandler();
+
+        public UsingAThread()
+        {
+            InitializeComponent();
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            Application.Run(new UsingAThread());
+        }
+
+        private void StartButton_Click(object sender, System.EventArgs e)
+        {
+            _Max = 100;
+            //Start thread
+
+            var t = new Thread(new ThreadStart(StartProcess));
+            t.Start();
+
+            MessageBox.Show("Done with operation!!");
+        }
+
+        private void StartProcess()
+        {
+            if (pbStatus.InvokeRequired)
+            {
+                var startProcess = new StartProcessHandler(StartProcess);
+                this.BeginInvoke(startProcess);//this means this Form invoke it on its own thread
+            }
+            else
+            {
+                this.Refresh();
+                this.pbStatus.Maximum = _Max;
+                for (int i = 0; i <= _Max; i++)
+                {
+                    Thread.Sleep(10);
+                    this.lblOutput.Text = i.ToString();
+                    this.pbStatus.Value = i;
+                }
+            }
+        }
+    }
+}
